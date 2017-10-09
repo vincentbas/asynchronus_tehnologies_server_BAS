396b95f02619349d5a50b611a831a08e312cc95b
Vincent Bas <vincentbas92@gmail.com>
2017-10-10 01:57:25 +0200

    correct jshint mistakes

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-var user = require("./user.js")
+var user = require("./user.js");
 
 module.exports = {
   logic: function (req, res) {
-    
-    console.log(req.url)
+
+    console.log(req.url);
     if (req.url == "/save") {
       user.save("testu", "testpwd", function() {
         res.writeHead(201, {'Content-Type': 'text/plain'});
         res.end('User is saved');
-      })
+      });
     } else if(req.url == "/get") {
       user.get("testu", function() {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('got the user');
-      })
+      });
     } else if(req.url == "/") {
       console.log("hello i'm in a function");
       res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -23,6 +23,6 @@ module.exports = {
       res.end('Route not found\n');
     }
 },
-port: "8888", 
+port: "8888",
 address: "127.0.0.1"
-}
+};
